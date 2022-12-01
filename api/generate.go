c5eddcff5a7a8357b7363ccb6b8eb0d956e1a1e9
Lars Karlslund <lars@karlslund.dk>
2022-12-01 14:26:29 +0100

    Updated generated files

--- api/generate.go
+++ api/generate.go
@@ -3,74 +3,146 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 
-	"github.com/deepmap/oapi-codegen/pkg/codegen"
 	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/lkarlslund/oapi-codegen/pkg/codegen"
 )
 
 type generatorinfo struct {
-	version  string
-	typename string
-	filename string
+	version   string
+	cachefile string
+	typename  string
+	filename  string
 }
 
 func main() {
 	versions := []generatorinfo{
 		{
-			version:  "1.0",
-			typename: "msgraph",
-			filename: "msgraph/types.go",
+			version:   "v1.0",
+			cachefile: "msgraphv10.json",
+			typename:  "msgraph",
+			filename:  "msgraph/types.go",
 		},
 		{
-			version:  "beta",
-			typename: "msgraphbeta",
-			filename: "msgraphbeta/types.go",
+			version:   "beta",
+			cachefile: "msgraphbeta.json",
+			typename:  "msgraphbeta",
+			filename:  "msgraphbeta/types.go",
 		},
 	}
 
 	for _, version := range versions {
-		err := generatetypes(version.version, version.typename, version.filename)
+		err := generatetypes(version)
 		if err != nil {
 			fmt.Printf("Error generating types for version %v: %v", version, err)
 		}
 	}
 }
 
-func generatetypes(version, typename, filename string) error {
+func generatetypes(gi generatorinfo) error {
 	// set up codegen
-	opts := codegen.Options{
-		GenerateTypes: true,
+	opts := codegen.Configuration{
+		PackageName: gi.typename,
+		Generate: codegen.GenerateOptions{
+			Models:       true,
+			Client:       false,
+			EmbeddedSpec: false,
+		},
+		// Compatibility: codegen.CompatibilityOptions{
+		// 	OldAliasing: true,
+		// },
 	}
 
-	resp, err := http.Get("https://graphexplorerapi.azurewebsites.net/openapi?operationIds=*&openApiVersion=3&graphVersion=" + version + "&format=json")
-	if err != nil {
-		// handle error
-		fmt.Printf("%s", err)
-	}
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		// handle error
-		return err
+	var data []byte
+	if f, err := os.Open(gi.cachefile); err == nil {
+		log.Println("Loading cached schema from", gi.cachefile)
+		data, err = ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+	} else {
+		log.Println("Fetching schema from Graph Explorer API")
+		resp, err := http.Get("https://graphexplorerapi.azurewebsites.net/openapi?operationIds=*&openApiVersion=3&graphVersion=" + gi.version + "&format=json")
+		if err != nil {
+			// handle error
+			fmt.Printf("%s", err)
+		}
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// handle error
+			return err
+		}
+		f, err := os.Create(gi.cachefile)
+		if err == nil {
+			f.Write(data)
+			f.Close()
+		}
 	}
 
 	loader := openapi3.NewLoader()
-	swagger, err := loader.LoadFromData([]byte(prefixup(string(data))))
 
-	code, err := codegen.Generate(swagger, typename, opts)
-	err = ioutil.WriteFile(filename, []byte(postfixup(code)), 0644)
-	return err
+	data = []byte(prefixup(string(data)))
+
+	log.Println("Loading swagger data")
+	swagger, err := loader.LoadFromData(data)
+	if err != nil {
+		return err
+	}
+
+	// ctx := context.Background()
+	// log.Println("Validating swagger data")
+	// err = swagger.Validate(ctx, openapi3.DisableSchemaPatternValidation())
+	// if err != nil {
+	// 	return err
+	// }
+
+	log.Println("Generating code")
+	code, err := codegen.Generate(swagger, opts)
+
+	// code = postfixup(code)
+
+	// Save it anyway
+	err2 := ioutil.WriteFile(gi.filename, []byte(code), 0644)
+	if err != nil {
+		return err
+	}
+	log.Println("Done")
+	return err2
 }
 
 var regexpFixup = regexp.MustCompile(`(?m)"(any|one)Of": \[\n\s+{\n\s+"\$ref": "([^"]+)"\n\s+}\n\s+\]`)
 
+var regexpFixupSchema = regexp.MustCompile(`"\$ref": "#/components/schemas/([^"]+)"`)
+
 func prefixup(input string) string {
-	result := strings.ReplaceAll(input, `"format": "decimal"`, `"format": "double"`)
-	result = strings.ReplaceAll(result, ` NaN`, ` "NaN C'mon Microsoft"`)
-	result = regexpFixup.ReplaceAllString(result, `"$$ref": "$2"`)
-	return result
+	input = regexpFixupSchema.ReplaceAllString(input, `"\$ref": "#/components/schemas/schema.$1"`)
+	input = strings.ReplaceAll(input, `"200": {`, `"Response200": {`)
+	// replaced := map[string]struct{}{}
+	// for _, match := range results {
+	// 	replace := match[1]
+	// 	if _, found := replaced[replace]; !found {
+	// 		strings.ReplaceAll(input, `"#/components/schemas/`+replace+`"`, `"#/components/schemas/schema.`+replace+`"`)
+	// 		replaced[replace] = struct{}{}
+	// 	}
+	// }
+	// input = strings.ReplaceAll(input, `StringCollectionResponse`, `microsoft.graph.stringCollectionResponse`) // Not supported, ignore it
+	// input = strings.ReplaceAll(input, `microsoft.graph.accessPackageAssignmentCollectionResponse`, `microsoft.graph.accessPackageAssignmentCollectionResponse2`) // Not supported, ignore it
+
+	// input = strings.Replace(input, `"microsoft.graph.accessPackageAssignmentCollectionResponse": {`, `"microsoft.graph.accessPackageAssignmentCollectionResponse": {
+	// "x-go-name": "Something",`, 1)
+
+	// input = strings.ReplaceAll(input, `"format": "base64url"`, `"format": ""`) // Not supported, ignore it
+	// input = strings.ReplaceAll(input, `"format": "int16"`, `"format": ""`)     // Not supported, ignore it
+
+	input = strings.ReplaceAll(input, `"format": "decimal"`, `"format": "double"`)
+	// result = strings.ReplaceAll(result, ` NaN`, ` "NaN C'mon Microsoft"`)
+	// result = regexpFixup.ReplaceAllString(result, `"$$ref": "$2"`)
+	return input
 }
 
 var regexpPostFixup = regexp.MustCompile(`([ \t]+[^\s]+)(.+json:"\S*_)`)
--- go.mod
+++ go.mod
@@ -1,42 +1,43 @@
 module github.com/lkarlslund/azureimposter
 
-go 1.17
+go 1.19
 
 require (
-	github.com/AzureAD/microsoft-authentication-library-for-go v0.3.1
-	github.com/deepmap/oapi-codegen v1.9.0
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/lkarlslund/lorca v0.1.11-0.20211114123408-2172618a4d98
+	github.com/lkarlslund/oapi-codegen v0.0.0-20221201112037-969a837755c4
 	github.com/lkarlslund/stringsplus v0.0.0-20211104080454-45e60fe6edc0
 	github.com/mozilla/tls-observatory v0.0.0-20210609171429-7bc42856d2e5
-	github.com/tidwall/gjson v1.11.0
+	github.com/tidwall/gjson v1.14.4
 )
 
 require (
-	github.com/getkin/kin-openapi v0.80.0 // indirect
-	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/invopop/yaml v0.2.0 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+)
+
+require (
+	github.com/getkin/kin-openapi v0.108.0
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
-	github.com/go-openapi/swag v0.19.5 // indirect
-	github.com/gofrs/uuid v4.2.0+incompatible
-	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
-	github.com/google/uuid v1.1.1 // indirect
-	github.com/kylelemons/godebug v1.1.0 // indirect
-	github.com/labstack/echo/v4 v4.2.1 // indirect
-	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/labstack/echo/v4 v4.9.1 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/mod v0.4.1 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20211031064116-611d5d643895 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.0.0-20210114065538-d78b04bdf963 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
